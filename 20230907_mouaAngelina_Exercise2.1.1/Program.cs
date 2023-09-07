d87a2a33a07460b017c87a47d0a1fe6dd2aadf5e
mouaa <mouaa@DESKTOP-NT2AJPK>
2023-09-07 16:22:05 -0400

    Add project files.

---
+++ 20230907_mouaAngelina_Exercise2.1.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InchesToCentimetersInteractive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double cent = 2.54;
+            Console.WriteLine("How many inches do you want to convert to centimeters?");
+            int inch = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(inch + " inches is " + cent * inch + " centimeters");
+            Console.ReadLine();
+        }
+    }
+}
